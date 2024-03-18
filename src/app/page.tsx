723e6181be57138fc8865209aa1b738c81a2e026
Adam Filiz <filizadam@gmail.com>
2024-03-17 22:32:37 -0700

    Upgraded react-table to v8 for typescript compat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,191 @@
 "use client";
 
 import Image from "next/image";
-import * as React from "react";
+import React, { HTMLAttributes, HTMLProps } from "react";
 import fakedata from "../mock_data.json";
-import { useTable } from "react-table";
+import { 
+  useReactTable,
+  flexRender, 
+  getCoreRowModel,
+  getSortedRowModel, 
+  createColumnHelper
+} from "@tanstack/react-table";
+
+type Jet = {
+  id: number;
+  name: string;
+  wingspan: number;
+  engines: number;
+  year: number;
+};
+
+function IndeterminateCheckbox({
+  indeterminate,
+  className = '',
+  ...rest
+}: { indeterminate?: boolean } & HTMLProps<HTMLInputElement>) {
+  const ref = React.useRef<HTMLInputElement>(null!)
+
+  React.useEffect(() => {
+    if (typeof indeterminate === 'boolean') {
+      ref.current.indeterminate = !rest.checked && indeterminate
+    }
+  }, [ref, indeterminate])
+
+  return (
+    <input
+      type="checkbox"
+      ref={ref}
+      className={className + ' cursor-pointer'}
+      {...rest}
+    />
+  )
+}
 
 export default function Home() {
-  console.log(fakedata);
-  const data = React.useMemo(() => fakedata, []);
-  const columns = React.useMemo(() => [
-    {
-      Header: "Name",
-      accessor: "name",
-    },
-    {
-      Header: "Wingspan",
-      accessor: "wingspan",
-    },
-    {
-      Header: "Engines",
-      accessor: "engines",
-    },
-    {
-      Header: "Year",
-      accessor: "year",
-    },
-  ], []);
+  const data: Jet[] = fakedata;
+  const columnHelper = createColumnHelper<Jet>();
+  const [rowSelection, setRowSelection] = React.useState({});
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = 
-    useTable({ columns, data });
+  const columns = [
+    columnHelper.display( {
+      id: 'selection',
+      cell: ({ row }) => (
+        <div className="px-1">
+          <IndeterminateCheckbox
+            {...{
+              checked: row.getIsSelected(),
+              disabled: !row.getCanSelect(),
+              indeterminate: row.getIsSomeSelected(),
+              onChange: row.getToggleSelectedHandler(),
+            }}
+          />
+        </div>
+      ),
+    }),
+    columnHelper.accessor('name', {
+      header: 'Name',
+      cell: info => info.getValue(),
+      enableSorting: false,
+    }),
+    columnHelper.accessor('wingspan', {
+      header: 'Wingspan',
+      cell: info => info.getValue(),
+      sortingFn: 'basic',
+      sortDescFirst: true,
+    }),
+    columnHelper.accessor('engines', {
+      header: 'Engines',
+      cell: info => info.getValue(),
+      enableSorting: false,
+    }),
+    columnHelper.accessor('year', {
+      header: 'Year',
+      cell: info => info.getValue(),
+      enableSorting: false,
+    })
+  ]
+
+
+  const table = useReactTable({
+    columns: columns,
+    data: data,
+    initialState: {
+      sorting: [
+        { id: 'wingspan', desc: true }
+      ]
+    },
+    state: {
+      rowSelection,
+    },
+    enableRowSelection: true,
+    onRowSelectionChange: setRowSelection,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>testing 1 2 3</div>
-      <table {...getTableProps()}>
+      <table>
         <thead>
-          {headerGroups.map((headerGroup: any) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column: any) => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-              ))}
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div
+                        className={
+                          header.column.getCanSort()
+                            ? 'cursor-pointer select-none'
+                            : ''
+                        }
+                        onClick={header.column.getToggleSortingHandler()}
+                        title={
+                          header.column.getCanSort()
+                            ? header.column.getNextSortingOrder() === 'asc'
+                              ? 'Sort ascending'
+                              : header.column.getNextSortingOrder() === 'desc'
+                                ? 'Sort descending'
+                                : 'Clear sort'
+                            : undefined
+                        }
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {{
+                          asc: ' ▲',
+                          desc: ' ▼',
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                    )}
+                  </th>
+                )
+              })}
             </tr>
           ))}
         </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row: any) => {
-            prepareRow(row);
+        <tbody>
+          {table.getRowModel().rows.map(row => {
             return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell: any) => {
-                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              <tr key={row.id}>
+                {row.getVisibleCells().map(cell => {
+                  return (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  )
                 })}
               </tr>
-            );
+            )
           })}
         </tbody>
       </table>
+      <div>
+        <button
+          className="border rounded p-2 mb-2"
+          onClick={() =>
+            console.info(
+              'table.getSelectedRowModel().flatRows',
+              table.getSelectedRowModel().flatRows
+            )
+          }
+        >
+          Log table.getSelectedRowModel().flatRows
+        </button>
+      </div>
+      <div>
+        <label>Row Selection State:</label>
+        <pre>{JSON.stringify(table.getState().rowSelection, null, 2)}</pre>
+      </div>
     </main>
   );
+
+
 }
